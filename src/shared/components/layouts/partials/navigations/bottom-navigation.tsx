0d375bd6880b81fe149cba42bab9d2dba7454ba6
dreykovic <amonaaudrey16@gail.com>
2024-08-28 18:07:35 +0000

    bottom custom

--- src/shared/components/layouts/partials/navigations/bottom-navigation.tsx
+++ src/shared/components/layouts/partials/navigations/bottom-navigation.tsx
@@ -11,27 +11,33 @@ const BottomNavigation = (props: Props) => {
   const { pageTitle, pageType } = useSelector(
     (state: RootState) => state.header,
   );
+
   return (
     <div
-      className={`btm-nav  ${pageType === 'simple' ? 'sticky' : ''} laptop:hidden bg-base-300 m-0 bottom-0 p-0 rounded-t-lg`}
+      className={`btm-nav  ${pageType === 'simple' ? 'sticky' : ''} laptop:hidden bg-base-300 m-0 bottom-0 p-0 rounded-t-2xl`}
     >
       {props.navs.map((nav, k) => {
         return (
-          <button
-            key={k + 1}
-            className={`${
-              nav.label === pageTitle
-                ? 'border-0 bg-base-200 text-primary  btn-square rounded-lg'
-                : ''
-            } mx-1`}
-          >
+          <button key={k + 1} className={` mx-1`}>
             <Link
               to={nav.path}
-              className={`flex flex-col items-center tooltip `}
+              className={`flex flex-col items-center absolute tooltip bottom-1`}
               data-tip={nav.label}
             >
-              <div>{nav.icon}</div>
-
+              {/* <div
+                className={`${nav.label === pageTitle ? activeClassNames : ''} `}
+              >
+                <div className="ring ring-neutral">{nav.icon}</div>
+              </div> */}
+              <div
+                className={`${nav.label === pageTitle ? 'avatar placeholder -translate-y-4 transform-gpu' : ''} `}
+              >
+                <div
+                  className={`${nav.label === pageTitle ? 'ring-neutral w-12 rounded-full ring ring-offset-2 ring-offset-neutral bg-primary' : ''} `}
+                >
+                  {nav.icon}
+                </div>
+              </div>
               <span className="text-[8px]">{nav.label}</span>
             </Link>
           </button>
