7f9a5a5935abd95418184e19dc9f2143eed08fcc
dreykovic <amonaaudrey16@gail.com>
2024-09-24 11:07:18 +0000

    ...

--- src/pages/order/components/index.tsx
+++ src/pages/order/components/index.tsx
@@ -181,7 +181,7 @@ const Form: React.FC<FormPropsType> = (prop: FormPropsType) => {
       console.error(`Error Lors du ${prop.order_type}:`, error);
       MySwal.fire({
         title: 'Erreur',
-        text: `Une erreur est survenue lors du ${prop.order_type}. \n ${error}`,
+        text: `Une erreur est survenue lors du ${prop.order_type}. \n ${client}`,
         icon: 'error',
         confirmButtonText: 'Réessayer',
 
