963d4422022aeb0c0150a4c00519719d0e6795e9
dreykovic <amonaaudrey16@gail.com>
2024-10-04 18:40:47 +0000

    historique

--- src/features/historique/components/index.tsx
+++ src/features/historique/components/index.tsx
@@ -1,17 +1,17 @@
 import './style.css';
 import { motion, Variants } from 'framer-motion';
 import { useState, useEffect, MutableRefObject } from 'react';
+
 import * as Icon from 'react-bootstrap-icons';
-import { useDispatch } from 'react-redux';
 
 import { setNavStatus } from '@/components/navigations/nav-slice';
-import SuspenseContent from '@/components/ui/suspens-content';
+
 import useBoundingClientRect from '@/hooks/use-bounding-client-rect';
 import useWindowDimensions from '@/hooks/use-window-dimensions';
-import { AppDispatch } from '@/stores';
-import { Order } from '@/types/models-interfaces';
 
-import { useGetOrderListQuery } from '../api';
+import { Order } from '@/types/models-interfaces';
+import { AppDispatch } from '@/stores';
+import { useDispatch } from 'react-redux';
 
 const listVariants: Variants = {
   visible: {
@@ -34,21 +34,20 @@ const itemVariants: Variants = {
   hidden: { opacity: 0, x: -100 },
 };
 
-const History = () => {
+type HistoryDataType = {
+  orders: Order[];
+  isLoading?: boolean;
+};
+
+const History: React.FC<HistoryDataType> = (props: HistoryDataType) => {
   const { height } = useWindowDimensions();
   const [rect, ref] = useBoundingClientRect<HTMLDivElement>();
   const [contentHeight, setContentHeight] = useState<number>(0);
+  const dispatch = useDispatch<AppDispatch>();
+  console.log(props.orders);
 
   const [lastScrollTop, setLastScrollTop] = useState<number>(0);
-  const dispatch = useDispatch<AppDispatch>();
-  const { data: orderList, isLoading } = useGetOrderListQuery(
-    {
-      chat_id: '7332983362',
-    },
-    {
-      refetchOnMountOrArgChange: true,
-    },
-  );
+
   const handleScroll = () => {
     if (ref.current) {
       const scrollTop = ref.current.scrollTop;
@@ -76,20 +75,7 @@ const History = () => {
   }, [lastScrollTop, height, rect, ref]);
 
   return (
-    <div className="h-full ">
-      <div className="flex my-1 p-1 text-base-300 justify-between gap-4 items-center ">
-        <Icon.Sliders className="w-6 h-6" />
-        <div className="flex justify-between items-center gap-4 my-4">
-          <div className="btn rounded-full text-success shadow-lg shadow-success">
-            <Icon.BuildingFillDown className="w-6 h-6" />
-          </div>
-          <div className="btn rounded-full text-warning">
-            <Icon.BuildingFillDown className="w-6 h-6" />
-          </div>
-        </div>
-        <Icon.Search className="w-6 h-6" />
-      </div>
-
+    <>
       <motion.div
         className="p-2 overflow-y-auto pb-2 "
         initial="hidden"
@@ -99,41 +85,55 @@ const History = () => {
         style={{ height: `${contentHeight}px` }}
         ref={ref as MutableRefObject<HTMLDivElement>}
       >
-        {isLoading ? (
-          <SuspenseContent />
-        ) : (
-          orderList?.map((historyItem: Order, index: number) => (
-            <motion.div key={index} variants={itemVariants}>
-              <div className="flex items-center p-3 bg-base-200 rounded-box gap-4 justify-between mb-3 history-badge history-badge-success relative">
-                <div className="flex items-center gap-5">
-                  <div className="avatar placeholder">
-                    <div className="ring-success-content w-12 rounded-full ring ring-offset-2 bg-success">
-                      <Icon.BuildingFillDown className="w-6 h-6" />
-                    </div>
+        {props.orders?.map((historyItem: Order, index: number) => (
+          <motion.div key={index} variants={itemVariants}>
+            <div
+              className={`flex items-center p-3 bg-base-200 rounded-box gap-4 justify-between mb-3 history-badge  relative    ${historyItem.state === 'COMMING' ? 'history-badge-info' : historyItem.state === 'CANCELLED' ? 'history-badge-error' : historyItem.order_type === 'DEPOT' ? 'history-badge-success' : 'history-badge-warning'}`}
+            >
+              <div className="flex items-center gap-5">
+                <div className="avatar placeholder">
+                  <div
+                    className={`ring-success-content w-12 rounded-full ring ring-offset-2  ${historyItem.state === 'COMMING' ? 'bg-info' : historyItem.state === 'CANCELLED' ? 'bg-error' : historyItem.order_type === 'DEPOT' ? 'bg-success' : 'bg-warning'}`}
+                  >
+                    <Icon.BuildingFillDown className="w-6 h-6" />
                   </div>
-                  <div>
-                    <div className="flex items-center gap-1">
-                      <Icon.WalletFill />
-                      <span>{historyItem.bookmaker_identifiant}</span>
-                    </div>
-                    <div className="text-[12px]">Payement Reussi</div>
-                    <div className="text-[8px] flex items-center gap-2">
+                </div>
+                <div>
+                  <div className="flex items-center gap-1">
+                    <Icon.WalletFill />
+                    <span>{historyItem.bookmaker_identifiant}</span>
+                  </div>
+                  <div className="text-[12px]">Payement Reussi</div>
+                  <div className="text-[8px] flex items-center gap-2">
+                    {historyItem.state === 'COMMING' ? (
+                      <div className="bg-info flex justify-center items-center w-4 h-4 rounded-full">
+                        <Icon.Clock className="w-2 h-2" />
+                      </div>
+                    ) : historyItem.state === 'CANCELLED' ? (
+                      <div className="bg-error flex justify-center items-center w-4 h-4 rounded-full">
+                        <Icon.X className="w-2 h-2" />
+                      </div>
+                    ) : historyItem.order_type === 'DEPOT' ? (
                       <div className="bg-success flex justify-center items-center w-4 h-4 rounded-full">
                         <Icon.Check className="w-2 h-2" />
                       </div>
-                      effectué le 22/20/021
-                    </div>
+                    ) : (
+                      <div className="bg-warning flex justify-center items-center w-4 h-4 rounded-full">
+                        <Icon.Check className="w-2 h-2" />
+                      </div>
+                    )}
+                    effectué le 22/20/021
                   </div>
                 </div>
-                <div className="btn rounded-full bg-base-300 p-2">
-                  {historyItem.montant}
-                </div>
               </div>
-            </motion.div>
-          ))
-        )}
+              <div className="btn rounded-full bg-base-300 p-2">
+                {historyItem.montant}
+              </div>
+            </div>
+          </motion.div>
+        ))}
       </motion.div>
-    </div>
+    </>
   );
 };
 
--- src/features/historique/index.tsx
+++ src/features/historique/index.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { setPageTitle, setPageType } from '@/components/header/header-slice';
 import useWindowDimensions from '@/hooks/use-window-dimensions';
 import { AppDispatch } from '@/stores';
-
+import * as Icon from 'react-bootstrap-icons';
 import History from './components';
+import { useGetOrderListQuery } from './api';
 
 const Historique: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -14,11 +15,16 @@ const Historique: React.FC = () => {
   const navigate = useNavigate();
   const { width } = useWindowDimensions();
 
+  const [orderType, setOrderType] = useState<'RETRAIT' | 'DEPOT'>('DEPOT');
+  const { data: orderList, isLoading } = useGetOrderListQuery(
+    {
+      chat_id: '12354867',
+    },
+    {
+      refetchOnMountOrArgChange: true,
+    },
+  );
   useEffect(() => {
-    // if (width >= 1024) {
-    //   navigate('/');
-    // }
-
     dispatch(setPageTitle({ title: 'Historique' }));
     dispatch(setPageType({ type: 'main' }));
   }, [dispatch, navigate, width]);
@@ -26,7 +32,42 @@ const Historique: React.FC = () => {
   return (
     <>
       <div className="p-4 overflow-hidden h-full">
-        <History />
+        <div className="h-full ">
+          <div className="flex my-1 p-1 text-base-300 justify-between gap-4 items-center ">
+            <Icon.Sliders className="w-6 h-6" />
+            <div className="flex justify-between items-center gap-4 my-4">
+              <button
+                className={`btn rounded-full text-success ${orderType === 'DEPOT' ? 'shadow-lg shadow-success' : ''}`}
+                onClick={() => {
+                  setOrderType('DEPOT');
+                }}
+              >
+                <Icon.BuildingFillDown className="w-6 h-6" />
+              </button>
+              <button
+                className={`btn rounded-full text-warning ${orderType === 'RETRAIT' ? 'shadow-lg shadow-warning' : ''}`}
+                onClick={() => {
+                  setOrderType('RETRAIT');
+                }}
+              >
+                <Icon.BuildingFillDown className="w-6 h-6" />
+              </button>
+            </div>
+            <Icon.Search className="w-6 h-6" />
+          </div>
+          {isLoading ? (
+            <div className="w-full    flex items-center justify-center">
+              <span className="loading loading-spinner text-primary loading-lg"></span>
+            </div>
+          ) : (
+            <History
+              orders={
+                orderList?.filter((order) => order.order_type === orderType) ??
+                []
+              }
+            />
+          )}
+        </div>
       </div>
     </>
   );
