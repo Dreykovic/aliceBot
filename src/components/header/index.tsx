0cf06790aead810c9a8b9b112ed8a36b7318c273
Dreykovic <amonaaudrey16@gmail.com>
2024-12-08 09:03:36 +0000

    ...

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,7 @@
-// import Clock from  '@components/ui/clock';
-
-// import { useSelector } from 'react-redux';
-
-// import { RootState } from '@/stores';
-
 import HeaderLogo from './header-logo';
 import HeaderTitle from './header-title';
 
 function Header() {
-  // const { client } = useSelector((state: RootState) => state.user);
   return (
     <div className="navbar max-h-4 bg-base-100 sticky laptop:static right-0 top-0 bottom-0  z-10 shadow-md max-laptop:rounded-b-2xl laptop:border-0 bordered">
       {/* Menu toogle for mobile view or small screen */}
@@ -18,12 +11,7 @@ function Header() {
       <div className="navbar-center">
         <HeaderTitle />
       </div>
-      <div className="navbar-end">
-        {/* Notification icon */}
-        {/* <HeaderActions /> */}
-        {/* {client ? `${client?.username}` : 'Username'} */}
-        {/* <Clock /> */}
-      </div>
+      <div className="navbar-end"></div>
     </div>
   );
 }
